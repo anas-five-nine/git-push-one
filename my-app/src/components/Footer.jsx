f8449f89c95468afad9b5b372d53a00c0756e8c4
Anas Zafar <anas.amphersoft@gmail.com>
2026-02-16 23:00:25 +0500

    add p

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer () {
     return (
         <div className="">
             <h1>Footer</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet quasi officia adipisci!</p>
         </div>
     )
 }
